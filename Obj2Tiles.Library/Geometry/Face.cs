01c5e444a4fe643b1807e9de40e9c39c7271e50e
Samuel Rundel <samuel.rundel@live.de>
2023-01-31 17:19:14 -0800

    Combines Face and FaceT

--- Obj2Tiles.Library/Geometry/Face.cs
+++ Obj2Tiles.Library/Geometry/Face.cs
@@ -1,26 +1,67 @@
-namespace Obj2Tiles.Library.Geometry;
+﻿namespace Obj2Tiles.Library.Geometry;
 
 public class Face
 {
-
     public int IndexA;
     public int IndexB;
     public int IndexC;
+    
+    public int TextureIndexA;
+    public int TextureIndexB;
+    public int TextureIndexC;
+    
+    public int MaterialIndex;
+    public bool HasTexture { get; }
 
-    public override string ToString()
+    public Face(int indexA,
+        int indexB,
+        int indexC)
     {
-        return $"{IndexA} {IndexB} {IndexC}";
+        HasTexture = false;
+        IndexA = indexA;
+        IndexB = indexB;
+        IndexC = indexC;
+        
     }
-
-    public Face(int indexA, int indexB, int indexC)
+    
+    public Face(
+        int indexA,
+        int indexB,
+        int indexC,
+        int textureIndexA,
+        int textureIndexB,
+        int textureIndexC,
+        int materialIndex)
     {
         IndexA = indexA;
         IndexB = indexB;
         IndexC = indexC;
+
+        TextureIndexA = textureIndexA;
+        TextureIndexB = textureIndexB;
+        TextureIndexC = textureIndexC;
+
+        MaterialIndex = materialIndex;
+        HasTexture = true;
     }
 
-    public virtual string ToObj()
+    public override string ToString()
     {
+        if (HasTexture)
+        {
+            return $"{IndexA} {IndexB} {IndexC} | {TextureIndexA} {TextureIndexB} {TextureIndexC} | {MaterialIndex}";
+        }
+        return $"{IndexA} {IndexB} {IndexC}";
+
+    }
+    
+    public string ToObj()
+    {
+        if (HasTexture)
+        {
+            return
+                $"f {IndexA + 1}/{TextureIndexA + 1} {IndexB + 1}/{TextureIndexB + 1} {IndexC + 1}/{TextureIndexC + 1}";
+        }
         return $"f {IndexA + 1} {IndexB + 1} {IndexC + 1}";
     }
 }
